b67619c95c785d10a6dc0b551a2604fb81bcb6fe
Choi5903 <cmg9431@gmail.com>
2024-09-05 09:50:02 +0900

    [Feat] 이동 변수 생성

--- Assets/Script/MoveTest.cs
+++ Assets/Script/MoveTest.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoveTest : MonoBehaviour
 {
-    public class Transform MoveOffset;
+    public Vector3 MoveOffset;
     // Start is called before the first frame update
     void Start()
     {
